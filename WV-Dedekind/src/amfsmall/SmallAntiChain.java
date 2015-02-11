4065352a674aab3ba5ee411134856f541dfd4547
mrTsjolder <mrTsjolder@users.noreply.github.com>
2015-02-11 16:45:35 +0100

    Finished first version of SmallAntiChain, but still need to get some more information on everything

--- WV-Dedekind/src/amfsmall/SmallAntiChain.java
+++ WV-Dedekind/src/amfsmall/SmallAntiChain.java
@@ -1,8 +1,14 @@
 package amfsmall;
 
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+
+import auxiliary.Pair;
 
 /**
  * More efficient representation for AntiChains.
@@ -28,7 +34,6 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 	 * Create an antichain with another antichain.
 	 */
 	public SmallAntiChain(SmallAntiChain ac) {
-		//TODO: clone?
 		theAntiChain = (BitSet) ac.theAntiChain.clone();
 		this.setUniverse(ac.getUniverse());
 	}
@@ -83,11 +88,17 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return result;
 	}
 	
-//	public static void makeAntiChain(SmallAntiChain pseudoAC) {
-//		for(int i = pseudoAC.theAntiChain.nextSetBit(0); i >= 0; i = pseudoAC.theAntiChain.nextSetBit(i+1)) {
-//			//TODO:?
-//		}
-//	}
+	/**
+	 * Decode the BigInteger b into an antichain
+	 * @param 	b 
+	 * 			The encoding for an antichain
+	 * @return	The SmallAntiChain from the given encoding
+	 */
+	public static SmallAntiChain decode(BigInteger b) {
+		SmallAntiChain result = new SmallAntiChain();
+		result.theAntiChain = BitSet.valueOf(b.toByteArray());
+		return result;
+	}
 	
 	/********************************************************
 	 * Utilities											*
@@ -124,10 +135,42 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 	}
 	
 	/**
+	 * Remove a set from this antichain
+	 * 
+	 * @param 	s
+	 * 			the set to be removed from this antichain
+	 * @return 	true if set was succesfully deleted, false when this antichain didn't change
+	 */
+	public boolean remove(SmallBasicSet s) {
+		int temp = s.toIntRepresentation();
+		if(!theAntiChain.get(temp))
+			return false;
+		theAntiChain.set(temp, false);
+		return true;
+	}
+	
+	/**
+	 * Remove all sets in the collection from this antichain
+	 * 
+	 * @param 	c
+	 * 			the collection of sets to be removed
+	 * @return	true if something has been deleted, false otherwise
+	 */
+	public boolean removeAll(SmallAntiChain c) {
+		boolean result = false;
+		for(int i = c.theAntiChain.nextSetBit(0); i >= 0; i = c.theAntiChain.nextSetBit(i+1)) {
+			if(remove(new SmallBasicSet(i)) && !result)
+				result = true;
+		}
+		return result;
+	}
+	
+	/**
 	 * Add a set to this antichain, so that the resulting antichain is 
 	 * the supremum of the union of this and {x}.
 	 * 
-	 * @param x
+	 * @param 	x
+	 * 			The set to be added
 	 */
 	private void addConditionally(SmallBasicSet x) {
 		SmallBasicSet a;
@@ -139,19 +182,45 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		theAntiChain.set(x.toIntRepresentation());
 	}
 
+	/**
+	 * Add elements of x that are not contained in this
+	 * remove elements of this that are contained in a set in x
+	 * 
+	 * @param 	x
+	 * 			The antichain to be added
+	 */
 	private void addConditionallyAll(SmallAntiChain x) {
 		for(int i = x.theAntiChain.nextSetBit(0); i >= 0; i = x.theAntiChain.nextSetBit(i+1)) {
 			addConditionally(new SmallBasicSet(i));
 		}
 	}
 	
-	//TODO: efficiency?
+	/**
+	 * Compute the union of all sets in this SmallAntiChain
+	 * 
+	 * @return a SmallBasicSet containing the union of all SmallBasicSets in this
+	 */
 	public SmallBasicSet sp() {
 		SmallBasicSet span = SmallBasicSet.emptySet();
 		for(int i = theAntiChain.nextSetBit(0); i >= 0; i = theAntiChain.nextSetBit(i+1)) {
 			span = span.union(new SmallBasicSet(i));
 		}
 		return span;
+	}
+	
+	/**
+	 * The projection on a specific dimension described by a SmallBasicSet
+	 * 
+	 * @param 	b
+	 * 			The set to project onto
+	 * @return 	the SmallAntiChain containing only intersections of this with b
+	 */
+	public SmallAntiChain project(SmallBasicSet b) {
+		SmallAntiChain res = SmallAntiChain.emptyAntiChain(this.getUniverse());
+		for (int i = theAntiChain.nextSetBit(0); i >= 0; i = theAntiChain.nextSetBit(i+1)) {
+			res.addConditionally(new SmallBasicSet(i).intersection(b));
+		}
+		return res;
 	}
 	
 	//TODO: useful?
@@ -165,6 +234,13 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return result;
 	}*/
 	
+	/**
+	 * Check whether this SmallAntiChain is greater or equal to the one with only x as an element
+	 * 
+	 * @param 	x
+	 * 			The one set of the other antichain
+	 * @return 	true iff x is contained in at least one set in this
+	 */
 	public boolean ge(SmallBasicSet x) {
 		for(int i = theAntiChain.nextSetBit(0); i >= 0; i = theAntiChain.nextSetBit(i+1)) {
 			if(new SmallBasicSet(i).hasAsSubset(x))
@@ -173,10 +249,211 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return false;
 	}
 	
+	/**
+	 * Check whether this SmallAntiChain is greater than other
+	 * 
+	 * @param 	other
+	 * 			The antichain to comare with
+	 * @return 	true iff all sets in other are contained in at least one set in this
+	 */
+	public boolean gt(SmallAntiChain x) {
+		return ge(x) && !equals(x);
+	}
+	
+	/**
+	 * Check whether this SmallAntiChain is less than other
+	 * 
+	 * @param 	other
+	 * 			The antichain to compare with
+	 * @return 	true iff all sets in this are contained in at least one set in other and other != this
+	 */
+	public boolean lt(SmallAntiChain x) {
+		return le(x) && !equals(x);
+	}
+	
+	/**
+	 * Minus for antichains: starting from the sets in this, remove all subsets of sets in f 
+	 * 
+	 * @param 	f
+	 * 			The antichain to substract from this
+	 * @return 	a SmallAntiChain containing all the sets in this 
+	 * 			which are not subsets of a set in f
+	 */
+	public SmallAntiChain minus(SmallAntiChain f) {
+		SmallAntiChain res = SmallAntiChain.emptyAntiChain(getUniverse());
+		for (int i = theAntiChain.nextSetBit(0); i >= 0; i = theAntiChain.nextSetBit(i+1)) {
+			SmallBasicSet x = new SmallBasicSet(i);
+			boolean found = false;
+			for (int j = theAntiChain.nextSetBit(0); j >= 0; j = theAntiChain.nextSetBit(j+1)) 
+				if (new SmallBasicSet(j).hasAsSubset(x)) found = true;
+			if (!found) res.add(x);
+		}
+		return res;
+	}
+	
+	/**
+	 * Find the minimal representation of this SmallAntiChain
+	 * under permutation of the elements
+	 * 
+	 * @return the representant with with the smallest encoding
+	 */
+	public SmallAntiChain standard() {
+		SmallBasicSet span = sp();
+		int map[] = new int[(int) span.size()];
+		int inverseMap[] = new int[span.maximum() + 1];
+		int pos = 0;
+		for (int i:span) {
+			map[pos] = i;
+			inverseMap[i] = pos++;
+		}
+		Iterator<Pair<int[], int[]>> permutations = MappingPermutation.getIterator(map,inverseMap,map.length);
+		SmallAntiChain best = this;
+		BigInteger bestCode = this.encode();
+		while (permutations.hasNext()) {
+			SmallAntiChain kand = this.map(permutations.next().snd);
+			BigInteger code = kand.encode();
+			if (code.compareTo(bestCode) < 0) {
+				best = kand;
+				bestCode = code;
+			}
+		}
+		return best;	
+	}
+	
+	/**
+	 * Find the symmetry group of this SmallAntiChain 
+	 * 
+	 * @return	the set of permutations of the elements 
+	 * 			under which this antichain is invariant
+	 */
+	public Set<int[]> symmetryGroup() {
+		Set<int[]> res = new HashSet<int[]>();
+		SmallBasicSet span = sp();
+		int map[] = new int[(int) span.size()];
+		int inverseMap[] = new int[span.maximum() + 1];
+		int pos = 0;
+		for (int i:span) {
+			map[pos] = i;
+			inverseMap[i] = pos++;
+		}
+		Iterator<Pair<int[], int[]>> permutations = MappingPermutation.getIterator(map,inverseMap,map.length);
+		BigInteger theCode = this.encode();
+		while (permutations.hasNext()) {
+			int[] p = permutations.next().snd;
+			SmallAntiChain kand = this.map(p);
+			BigInteger code = kand.encode();
+			if (code.compareTo(theCode) == 0) {
+				res.add(Arrays.copyOf(p, p.length));
+			}
+		}
+		return res;		
+	}
+	
+	/**
+	 * ???
+	 *
+	 * @param 	inverse
+	 * 			???
+	 * @return	???
+	 */
+	//TODO: complete comments
+	public SmallAntiChain map(int[] inverse) {
+		SmallAntiChain res = SmallAntiChain.emptyAntiChain(getUniverse());
+		for(int i = theAntiChain.nextSetBit(0); i >= 0; i = theAntiChain.nextSetBit(i+1)) {
+			res.add(new SmallBasicSet(i).map(inverse));
+		}
+		return res;
+	}
+	
+	/**
+	 * Encode this SmallAntiChain to a BigInteger
+	 * 
+	 * @return the binary encoding of this antichain
+	 */
+	public BigInteger encode() {
+		BigInteger result = BigInteger.ZERO;
+		for(int i = theAntiChain.nextSetBit(0); i >= 0; i = theAntiChain.nextSetBit(i+1))
+			result = result.setBit(i);
+		return result;
+	}
+	
+	/**
+	 * Reduce this with respect to the given span
+	 * to the unambiguous this = result[0].join(result[1].times({[m]})
+	 * with result[0].ge(result[1]) and m == span.maximum() 
+	 * 
+	 * @param 	span
+	 * 			The span to reduce with
+	 * @return	???
+	 */
+	//TODO: complete comments
+	public SmallAntiChain[] reduce(SmallBasicSet span) {
+		int m = span.maximum();
+		SmallBasicSet p = span.minus(m);
+		SmallAntiChain[] result = new SmallAntiChain[2];
+		result[0] = this.project(p);
+		SmallAntiChain a1 = new SmallAntiChain(this);
+		a1.removeAll(result[0]);
+		result[1] = a1.project(p);
+		return result;
+	}
+	
+	/**
+	 * omicron(tau, alfa) is the largest antichain chi le this for which chi.meet(alfa).equals(tau)
+	 * 
+	 * use omicron(alfa) in stead
+	 * 
+	 * @param 	tau
+	 * 			???
+	 * @param 	alfa
+	 * 			???
+	 * @pre 	tau.le(alfa)
+	 * @return	???
+	 */
+	//TODO: complete comments
+	@Deprecated
+	public SmallAntiChain omicron(SmallAntiChain tau, SmallAntiChain alfa) {
+		if (tau.equals(SmallAntiChain.emptyAntiChain())) {
+			if (alfa.equals(SmallAntiChain.emptyAntiChain())) return this;
+			else return SmallAntiChain.emptyAntiChain();
+		}
+		SmallAntiChain res = SmallAntiChain.emptyAntiChain(getUniverse());
+		res.add(sp().minus(alfa.sp()));
+		for (int i = alfa.theAntiChain.nextSetBit(0); i >= 0; i = alfa.theAntiChain.nextSetBit(i+1)) {
+			SmallAntiChain current = SmallAntiChain.emptyAntiChain(getUniverse());
+			current.add(new SmallBasicSet(i));
+			res = (SmallAntiChain) res.times(current.meet(tau));
+		}
+		return (SmallAntiChain) res.meet(this);
+	}
+	
+	/**
+	 * omicron(alfa) is the largest SmallAntiChain chi for which chi.meet(alfa).equals(this)
+	 * 
+	 * @param 	alfa
+	 * 			???
+	 * @pre 	this.le(alfa)
+	 * @return	
+	 */
+	//TODO: complete comments
+	public SmallAntiChain omicron(SmallAntiChain alfa) {
+		SmallAntiChain tauD = (SmallAntiChain) dual();
+		tauD.removeAll((SmallAntiChain) alfa.dual());
+		return (SmallAntiChain) tauD.dual();		
+	}
+	
 	/********************************************************
 	 * Implementing methods									*
 	 ********************************************************/
 	
+	/**
+	 * Sum of two SmallAntiChains is the antichain that contains all sets in the union except those that
+	 * are a subset of another set in this union
+	 * 
+	 * @param 	e
+	 * 			The antichain to be joined
+	 * @return 	sup(this union e)
+	 */
 	@Override
 	public LatticeElement join(LatticeElement e) {
 		SmallAntiChain result = new SmallAntiChain(this);
@@ -185,6 +462,14 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return result;
 	}
 
+	/**
+	 * Dot product of two SmallAntiChains is the function that contains all 
+	 * intersections of a set in this and a set in other
+	 * 
+	 * @param 	e
+	 * 			The antichain to be met
+	 * @return 	{A^B|A in this, B in e}
+	 */
 	@Override
 	public LatticeElement meet(LatticeElement e) {
 		SmallAntiChain result = SmallAntiChain.emptyAntiChain(this.getUniverse());
@@ -194,6 +479,14 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return result;
 	}
 
+	/**
+	 * Generalised product of two AntiChains is the largest function K 
+	 * such that P_sp(this)(K) <= this and P_sp(e)(K) <= e and K <= {sp(this.join(e))}
+	 * 
+	 * @param 	e
+	 * 			The other antichain to apply the binary operation
+	 * @effect 	max{K|P_sp(this)(K) <= this and P_sp(other)(K) <= e}
+	 */
 	@Override
 	public LatticeElement times(LatticeElement e) {
 		SmallAntiChain result = new SmallAntiChain(this);
@@ -216,6 +509,11 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return result;
 	}
 
+	/**
+	 * The dual of this SmallAntiChain in the lattice of antichains on its universe
+	 * 
+	 * @return 	sup{A|forall k in this : this.getUniverse()\k not in A}
+	 */
 	@Override
 	public LatticeElement dual() {
 		SmallAntiChain tres, result = SmallAntiChain.emptyAntiChain(this.getUniverse());
@@ -235,6 +533,13 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return result;
 	}
 
+	/**
+	 * Check whether this SmallAntiChain is greater or equal than other
+	 * 
+	 * @param 	other
+	 * 			The antichain to comare with
+	 * @return 	true iff all sets in other are contained in at least one set in this
+	 */
 	@Override
 	public boolean ge(LatticeElement e1) {
 		for(int i = ((SmallAntiChain) e1).theAntiChain.nextSetBit(0); i >= 0; i = ((SmallAntiChain) e1).theAntiChain.nextSetBit(i+1)) {
@@ -244,6 +549,13 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return true;
 	}
 
+	/**
+	 * Check whether this SmallAntiChain is less or equal than other
+	 * 
+	 * @param 	e1
+	 * 			The antichain to compare with
+	 * @return 	true iff all sets in this are contained in at least one set in other
+	 */
 	@Override
 	public boolean le(LatticeElement e1) {
 		boolean ok;
@@ -259,17 +571,46 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 		return true;
 	}
 
+	/**
+	 * Check wehter this SmallAntiChain equals the other
+	 * 
+	 * @param	e
+	 * 			The antichain to comare with
+	 * @return	true iff all sets in this are in all sets from the other
+	 */
 	@Override
 	public boolean equals(LatticeElement e) {
 		return theAntiChain.equals(((SmallAntiChain) e).theAntiChain);
 	}
 
+	/**
+	 * A lexicographic comparison of SmallAntiChains
+	 * 
+	 * @param 	o 
+	 * 			The SmallAntiChain to compare with
+	 * @return 	< 0 if this is l.g. before o, 
+	 * 			> 0 if this is l.g. after o, 
+	 * 			= 0 otherwise
+	 */
 	@Override
 	public int compareTo(SmallAntiChain o) {
-		// TODO Auto-generated method stub
-		return 0;
+		Iterator<SmallBasicSet> s1 = this.iterator();
+		Iterator<SmallBasicSet> s2 = o.iterator();
+		while (s1.hasNext() && s2.hasNext()) {
+			int c = s1.next().compareTo(s2.next());
+			if (c != 0) return c;
+		}
+		if (s1.hasNext()) return 1;
+		else if (s2.hasNext()) return -1;
+		else return 0;
 	}
 
+	/**
+	 * Get an iterator to iterate over the sets in this antichain
+	 * 
+	 * @return	an iterator that iterates over the sets in the order
+	 * 			that follows from the representation from Carl Salaets
+	 */
 	@Override
 	public Iterator<SmallBasicSet> iterator() {
 		return new Iterator<SmallBasicSet>() {
@@ -290,7 +631,6 @@ public class SmallAntiChain implements Iterable<SmallBasicSet>, Comparable<Small
 			@Override
 			public void remove() {
 				// throw new NotImplementedException();
-				
 			}
 			
 		};
